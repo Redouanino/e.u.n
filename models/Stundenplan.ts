be26f0615ca555c19cb28e9864d35f7ae136c027
Redouanino <redouflsser@gmail.com>
2024-09-01 16:48:44 +0200

    secound commit

--- models/Stundenplan.ts
+++
@@ -1,9 +0,0 @@
-// models/Stundenplan.ts
-import mongoose from 'mongoose';
-
-const StundenplanSchema = new mongoose.Schema({
-    fach: { type: String, required: true },
-    kurse: [{ type: String, required: true }],
-});
-
-export default mongoose.models.Stundenplan || mongoose.model('Stundenplan', StundenplanSchema);
